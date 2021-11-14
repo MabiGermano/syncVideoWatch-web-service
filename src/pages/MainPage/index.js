c2ae4ed247db0fefe4aed29fbc218b1839616067
MabiGermano <beatriz.germano@nubank.com.br>
2021-11-13 22:38:38 -0300

    improving the frontend using material ui

--- src/pages/MainPage/index.js
+++ src/pages/MainPage/index.js
@@ -1,95 +1,265 @@
-import YouTube from 'react-youtube'
-import { useState } from 'react'
-import { PlayArrow, NavigateNext, NavigateBefore, Pause } from '@material-ui/icons'
 
-import { onPlayerStateChange, onReadyEvent, addToQueue } from '../../service/PlayerService'
-import { nextVideo } from '../../service/ControlsService'
+import { styled, useTheme } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import YouTube from "react-youtube";
 
+import { useState } from "react";
 
-import socketIOClient from 'socket.io-client'
-import './style.css'
+import Typography from '@mui/material/Typography';
+import PauseRounded from '@mui/icons-material/PauseRounded';
+import PlayArrowRounded from '@mui/icons-material/PlayArrowRounded';
+import FastForwardRounded from '@mui/icons-material/FastForwardRounded';
+import FastRewindRounded from '@mui/icons-material/FastRewindRounded';
 
-const ENDPOINT = 'http://localhost:3333'
-const socket = socketIOClient(ENDPOINT)
+import socketIOClient from "socket.io-client";
+import "./style.css";
+
+import PlayArrowIcon from '@mui/icons-material/PlayArrow';
+import ErrorOutlineIcon from '@mui/icons-material/ErrorOutline';
+import { Grid, IconButton, ImageListItem, List, ListItem, ListItemButton, ListItemText, Slider, Tooltip } from '@mui/material';
+import { Link } from 'react-router-dom';
+
+const ENDPOINT = "http://localhost:3333";
+const socket = socketIOClient(ENDPOINT);
+
+const TinyText = styled(Typography)({
+  fontSize: '0.75rem',
+  opacity: 0.38,
+  fontWeight: 500,
+  letterSpacing: 0.2,
+});
 
 function MainPage() {
-    const [doJump, setDoJump] = useState(false)
-    const [progressBar, setProgressBar] = useState({
-        max: 0,
-        value: 0
-    })
+  const [doJump, setDoJump] = useState(false);
+  const [progressBar, setProgressBar] = useState({
+    max: 0,
+    value: 0,
+  });
 
-    const [videoUrl, setVideoUrl] = useState("")
+  const [videoUrl, setVideoUrl] = useState("");
 
-    function buildServiceParams() {
-        return {
-            socket: socket,
-            progressBar: progressBar,
-            setProgressBar: setProgressBar,
-            doJump: doJump,
-            setDoJump: setDoJump
-        }
-    }
+  function buildServiceParams() {
+    return {
+      socket: socket,
+      progressBar: progressBar,
+      setProgressBar: setProgressBar,
+      doJump: doJump,
+      setDoJump: setDoJump,
+    };
+  }
 
-    function handdleVideoLinks(event) {
-        addToQueue(videoUrl)
-    }
+  function getPlaylist() {
+    return [{
+      "id": "1",
+      "title": "Dwayne Johnson - You're Welcome (from Moana/Official Video)",
+      "author_name": "DisneyMusicVEVO",
+      "author_url": "https://www.youtube.com/user/DisneyMusicVEVO",
+      "provider_url": "https://www.youtube.com/",
+      "thumbnail_url": "https://i.ytimg.com/vi/79DijItQXMM/hqdefault.jpg",
+      "video_code": " 79DijItQXMM"
+    }, {
+      "id": "2",
+      "title": "Dwayne Johnson - You're Welcome (from Moana/Official Video)",
+      "author_name": "DisneyMusicVEVO",
+      "author_url": "https://www.youtube.com/user/DisneyMusicVEVO",
+      "provider_url": "https://www.youtube.com/",
+      "thumbnail_url": "https://i.ytimg.com/vi/79DijItQXMM/hqdefault.jpg",
+      "video_code": " 79DijItQXMM"
+    }, {
+      "id": "3",
+      "title": "Dwayne Johnson - You're Welcome (from Moana/Official Video)",
+      "author_name": "DisneyMusicVEVO",
+      "author_url": "https://www.youtube.com/user/DisneyMusicVEVO",
+      "provider_url": "https://www.youtube.com/",
+      "thumbnail_url": "https://i.ytimg.com/vi/79DijItQXMM/hqdefault.jpg",
+      "video_code": " 79DijItQXMM"
+    }, {
+      "id": "4",
+      "title": "Dwayne Johnson - You're Welcome (from Moana/Official Video)",
+      "author_name": "DisneyMusicVEVO",
+      "author_url": "https://www.youtube.com/user/DisneyMusicVEVO",
+      "provider_url": "https://www.youtube.com/",
+      "thumbnail_url": "https://i.ytimg.com/vi/79DijItQXMM/hqdefault.jpg",
+      "video_code": " 79DijItQXMM"
+    }, {
+      "id": "5",
+      "title": "Dwayne Johnson - You're Welcome (from Moana/Official Video)",
+      "author_name": "DisneyMusicVEVO",
+      "author_url": "https://www.youtube.com/user/DisneyMusicVEVO",
+      "provider_url": "https://www.youtube.com/",
+      "thumbnail_url": "https://i.ytimg.com/vi/79DijItQXMM/hqdefault.jpg",
+      "video_code": " 79DijItQXMM"
+    }]
+  }
 
-    return (
-        <>
-            <main>
-                <div id="player">
-                    <YouTube containerClassName="video-container" videoId="GtvS897PiyQ"
-                        opts={{
-                            width: (window.innerWidth * 50) / 100,
-                            height: (window.innerHeight * 75) / 100,
-                            playerVars: { 'controls': 0 },
+
+  const theme = useTheme();
+  const duration = 200; // seconds
+  const [position, setPosition] = useState(32);
+  const [paused, setPaused] = useState(false);
+  function formatDuration(value) {
+    const minute = Math.floor(value / 60);
+    const secondLeft = value - minute * 60;
+    return `${minute}:${secondLeft < 9 ? `0${secondLeft}` : secondLeft}`;
+  }
+  const mainIconColor = theme.palette.mode === 'dark' ? '#fff' : '#000';
+  const lightIconColor =
+    theme.palette.mode === 'dark' ? 'rgba(255,255,255,0.4)' : 'rgba(0,0,0,0.4)';
+
+  return (
+    <>
+      <Box>
+        <Grid container spacing={2}>
+          <Grid item xs={4}>
+            <nav>
+              <List sx={{
+                overflowY: 'scroll'
+              }}>
+                {getPlaylist().map(video => {
+                  return (
+                    <ListItemButton
+                      sx={{
+                        display: 'flex',
+                        flexDirection: { xs: 'column', md: 'row' },
+                        alignItems: 'center',
+                        overflow: 'hidden',
+                        fontWeight: 'bold',
+                      }}>
+                      <Box
+                        component="img"
+                        sx={{
+                          height: 233,
+                          width: 350,
+                          maxHeight: { xs: 233, md: 167 },
+                          maxWidth: { xs: 350, md: 250 },
                         }}
-                        onReady={event => onReadyEvent(event, buildServiceParams())}
-                        onStateChange={event => onPlayerStateChange(event, buildServiceParams())}
+                        alt="The house from the offer."
+                        src={video.thumbnail_url}
+                      />
+
+                      <Box
+                        sx={{
+                          display: 'flex',
+                          flexDirection: 'column',
+                          alignItems: { xs: 'center', md: 'flex-start' },
+                          m: 3,
+                          minWidth: { md: 350 },
+                        }}
+                      >
+                        <ListItemText primary={video.title} sx={{
+                          textOverflow: 'ellipsis'
+                        }} />
+
+                        <Link to={video.author_url}>
+                          <ListItemText primary={video.author_name} />
+                        </Link>
+                      </Box>
+
+                    </ListItemButton>
+                  )
+                })}
+              </List>
+            </nav>
+          </Grid>
+
+          <Grid item xs={8}>
+            <Box>
+              <div id="player">
+                <YouTube
+                  containerClassName="video-container"
+                  videoId="79DijItQXMM"
+                  opts={{
+                    width: (window.innerWidth * 65.5) / 100,
+                    height: (window.innerHeight * 65.5) / 100,
+                    playerVars: { controls: 0 },
+                  }}
+                />
+              </div>
+
+
+
+
+
+              <Slider
+                aria-label="time-indicator"
+                size="small"
+                value={position}
+                min={0}
+                step={1}
+                max={duration}
+                onChange={(_, value) => setPosition(value)}
+                sx={{
+                  color: theme.palette.mode === 'dark' ? '#fff' : 'rgba(0,0,0,0.87)',
+                  height: 4,
+                  '& .MuiSlider-thumb': {
+                    width: 8,
+                    height: 8,
+                    transition: '0.3s cubic-bezier(.47,1.64,.41,.8)',
+                    '&:before': {
+                      boxShadow: '0 2px 12px 0 rgba(0,0,0,0.4)',
+                    },
+                    '&:hover, &.Mui-focusVisible': {
+                      boxShadow: `0px 0px 0px 8px ${theme.palette.mode === 'dark'
+                        ? 'rgb(255 255 255 / 16%)'
+                        : 'rgb(0 0 0 / 16%)'
+                        }`,
+                    },
+                    '&.Mui-active': {
+                      width: 20,
+                      height: 20,
+                    },
+                  },
+                  '& .MuiSlider-rail': {
+                    opacity: 0.28,
+                  },
+                }}
+              />
+              <Box
+                sx={{
+                  display: 'flex',
+                  alignItems: 'center',
+                  justifyContent: 'space-between',
+                  mt: -2,
+                }}
+              >
+                <TinyText>{formatDuration(position)}</TinyText>
+                <TinyText>-{formatDuration(duration - position)}</TinyText>
+              </Box>
+
+              <Box
+                sx={{
+                  display: 'flex',
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                  mt: -1,
+                }}
+              >
+                <IconButton aria-label="previous song">
+                  <FastRewindRounded fontSize="large" htmlColor={mainIconColor} />
+                </IconButton>
+                <IconButton
+                  aria-label={paused ? 'play' : 'pause'}
+                  onClick={() => setPaused(!paused)}
+                >
+                  {paused ? (
+                    <PlayArrowRounded
+                      sx={{ fontSize: '3rem' }}
+                      htmlColor={mainIconColor}
                     />
-
-                    <div>
-                        {/* <div className="input-group">
-                            <input type="text" onChange={ (e) => { setVideoUrl(e.target.value) } }/>
-                        </div>
-                        <button onClick={handdleVideoLinks}>Go!</button> */}
-                    </div>
-                </div>
-
-                <div id="queue">
-                    <div id="list">
-
-                    </div>
-                    <hr/>
-                    <div id="controls">
-                        <div className="progress">
-                            <input type="range" min="0" max={progressBar.max} value={progressBar.value} step="1" className="range blue" />
-                        </div>
-                        <div className="actions">
-                            <div className="button-container">
-                                <div className="arrows previous">
-                                    <NavigateBefore />
-                                </div>
-                            </div>
-                            <div className="button-container">
-                                <div class="play-pause"> 
-                                    <PlayArrow />
-                                </div>
-                            </div>
-                            <div className="button-container" onClick={nextVideo}>
-                                <div className="arrows next">
-                                    <NavigateNext />
-                                </div>
-                            </div>
-                        </div>
-
-                    </div>
-                </div>
-
-            </main>
-        </>
-    )
+                  ) : (
+                    <PauseRounded sx={{ fontSize: '3rem' }} htmlColor={mainIconColor} />
+                  )}
+                </IconButton>
+                <IconButton aria-label="next song">
+                  <FastForwardRounded fontSize="large" htmlColor={mainIconColor} />
+                </IconButton>
+              </Box>
+            </Box>
+          </Grid>
+        </Grid>
+      </Box>
+    </>
+  );
 }
 
-export default MainPage
+export default MainPage;
--- src/service/ControlsService.js
+++ src/service/ControlsService.js
@@ -1,47 +1,47 @@
-import { getPlayerInstance } from './PlayerService';
+// import { getPlayerInstance } from './PlayerService';
 
-// porque isso funciona dessa forma?
-export const CONTROLS = {
-    playPause: () => document.querySelector('.play-pause'),
-    progressBar: () => document.querySelector('.progress input[type=range]')
-}
+// // porque isso funciona dessa forma?
+// // export const CONTROLS = {
+// //     playPause: () => document.querySelector('.play-pause'),
+// //     progressBar: () => document.querySelector('.progress input[type=range]')
+// // }
 
-export function playPauseActions(player) {
+// export function playPauseActions(player) {
     
-    const { playPause } = CONTROLS;
-    playPause().addEventListener('click', (e) => {
-        const selfClassList = e.target.classList;
+//     // const { playPause } = CONTROLS;
+//     playPause().addEventListener('click', (e) => {
+//         const selfClassList = e.target.classList;
 
-        if(!selfClassList.contains('pause')) {
-            player.playVideo();
-            selfClassList.add('pause');
-        }else {
-            player.pauseVideo();
-            selfClassList.remove('pause');
-        }
+//         if(!selfClassList.contains('pause')) {
+//             player.playVideo();
+//             selfClassList.add('pause');
+//         }else {
+//             player.pauseVideo();
+//             selfClassList.remove('pause');
+//         }
 
-    });
-}
+//     });
+// }
 
-export function jumpVideo(player, setDoJump) {
+// // export function jumpVideo(player, setDoJump) {
     
-    const { progressBar } = CONTROLS;
-    const progressBarElement = progressBar();
-    progressBarElement.addEventListener('input', (e) => {
-        let v = progressBarElement.value;
-        setDoJump(true)
-        player.seekTo(v);     
-    });
-}
+// //     // const { progressBar } = CONTROLS;
+// //     const progressBarElement = progressBar();
+// //     progressBarElement.addEventListener('input', (e) => {
+// //         let v = progressBarElement.value;
+// //         setDoJump(true)
+// //         player.seekTo(v);     
+// //     });
+// // }
 
-export function nextVideo() {
-    getPlayerInstance().nextVideo();    
-}
+// export function nextVideo() {
+//     getPlayerInstance().nextVideo();    
+// }
 
-export function previousVideo() {
-    getPlayerInstance().previousVideo();
-}
+// export function previousVideo() {
+//     getPlayerInstance().previousVideo();
+// }
 
-export function playVideoAt(index) {
-    getPlayerInstance().playVideoAt(index);
-}
+// export function playVideoAt(index) {
+//     getPlayerInstance().playVideoAt(index);
+// }
